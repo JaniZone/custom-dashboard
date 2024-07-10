277e12ff6778c7bf8b6a5236abb55fa121424c17
JaniZone <u2103106@rajagiri.edu.in>
2024-07-10 23:30:03 +0530

    HELLO PADIK

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home(){
     <div>
       <h1>Welcome to Next.js with TypeScript</h1>
      <p>
-        <Link href="\hello">Go to Hello World Page</Link>
+        <Link href="\janis">Go to Hello World Page</Link>
       </p>
     </div>
   );
