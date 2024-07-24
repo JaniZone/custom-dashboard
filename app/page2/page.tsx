f1a96e9389dd2fae70c51d7746e6db2a61cf25a7
fathima nourin <fathimanourin802@gmail.com>
2024-07-24 12:27:20 +0530

    Merge branch 'main' of https://github.com/JaniZone/custom-dashboard into nourin

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -2,13 +2,14 @@
 import dynamic from 'next/dynamic';
 import styles from '../styles/dataupload.module.css';
 
-const DynamicFileInput = dynamic(() => import('../Dashboard/dataupload'), { ssr: false });
+const DynamicFileInput = dynamic(() => import('../project/dataupload'), { ssr: false });
 
 const Dataupload : React.FC = () => {
   
   return (
     <div className={styles.main}>
-      <h1>UPLOAD EXCEL FILES</h1>
+   <h1>UPLOAD EXCEL FILES</h1>
+
       <DynamicFileInput />
     </div>
   );
